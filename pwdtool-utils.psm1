26c99842dfc41083c163daf62ef62d565e54c201
Craig Debbo <cdebbo@kcdsb.on.ca>
2016-12-29 14:25:35 -0600

    new file. refactored the user validation

---
+++ pwdtool-utils.psm1
@@ -0,0 +1,20 @@
+﻿# pwdtool utilities
+import-module ActiveDirectory
+
+# username = user we want to validate
+# group = the user must be a member of this group in order to return true.
+# permission = ignored for now.
+function validateUser($username, $group, $permission) {
+  try {
+    #$username = $PoSHUsername.split('\\')[1]
+    if (((Get-ADGroupMember $group -Recursive).name -contains $(get-aduser $username).Name) -eq $false) {
+      return @{"valid" = $false; message = "Permissions Problem"}
+    }
+  } catch {
+    write-host ($_.Exception.Message);
+    return @{"valid" = $false; message = "Permissions Problem"}
+  }
+  return @{valid = $true; message = ""}
+}
+
+Export-ModuleMember -Function validateUser
